a4cf85a286d71988cc46621cd28acbd62bf0ea04
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-10-12 14:24:38 -0500

    CG 14-2

--- CalculatorUnitTests/Program.cs
+++ CalculatorUnitTests/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
 
-namespace CalculatorUnitTests
+namespace Calculator_CG_14_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+
+
+
+                //Create a basic calculator app that lets the user enter a basic formula 
+                //like 10 + 4. If the user enters in any invalid data, catch the error and 
+                //let the user know their input was bad.This application should be able to 
+                //do addition, subtraction, division and multiplication.You should also be 
+                //using a calculator class instead of putting everything in the Program class. 
+                //Upload your program to a new repository called CG 11-1
+
+
+                int firstnumber = 0;
+                int secondnumber = 0;
+                int answer = 0;
+                string operation = " ";
+
+                Console.WriteLine("Welcome to a simple calculator. Please enter your equation in the following format: number, space, operator, space, number, IE 2 + 2");
+
+
+                string equation = Console.ReadLine();
+
+                var parts = equation.Split(" ");
+
+                if (parts != null)
+                {
+                    firstnumber = Convert.ToInt32(parts[0]);
+                    operation = parts[1];
+                    secondnumber = Convert.ToInt32(parts[2]);
+
+                }
+
+                switch (operation)
+                {
+                    case "+":
+                        answer = Operators.Addition(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + "+" + secondnumber + "=" + answer); break;
+
+                    case "-":
+                        answer = Operators.Subtraction(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + "-" + secondnumber + "=" + answer); break;
+
+                    case "*":
+                        answer = Operators.Multiply(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + "*" + secondnumber + "=" + answer); break;
+
+                    case "/":
+                        answer = Operators.Division(firstnumber, secondnumber);
+                        Console.WriteLine(firstnumber + "/" + secondnumber + "=" + answer); break;
+
+                    default:
+
+                        Console.WriteLine("Invalid operator"); break;
+
+
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Incorrect format, try again and pay attention!");
+            }
+
+            Console.ReadLine();
+
         }
+
     }
 }
+
---
+++ CalculatorUnitTests/Testing.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator_CG_14_1
+{
+    class Operators
+    {
+        /// <summary>
+        /// adds 2 numbers
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns>2 numbers added</returns>
+        public static int Addition(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+        /// <summary>
+        /// subtracting num2 from num1
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns>num2 is subtracted from num1</returns>
+        public static int Subtraction(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+        /// <summary>
+        /// multiply 2 numbers
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns>num1 is multiplied by num2</returns>
+        public static int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }/// <summary>
+         /// 2 numbers divided
+         /// </summary>
+         /// <param name="num1"></param>
+         /// <param name="num2"></param>
+         /// <returns>num1 divided by num2</returns>
+        public static int Division(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+    }
+}
