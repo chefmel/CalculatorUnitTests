3345e2480d51ecaf26fcaef67a99ccda2d01d21b
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-10-12 11:44:40 -0500

    Add project files.

---
+++ CalculatorUnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorUnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
